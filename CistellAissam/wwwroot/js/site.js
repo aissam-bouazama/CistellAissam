dd5c696f495b7e5c34f3a88fd1574f730107359a
Aissam <abouzama@almata.cat>
2025-01-31 17:43:40 +0100

    finalitzar la part de finalitzar compra en procés de cambiar el estil i millor i documentar el codigo i optimitzar-lo

--- CistellAissam/wwwroot/js/site.js
+++ CistellAissam/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const menu =document.querySelector("nav-link")
+document.addEventListener("DOMContentLoaded", function (e) {
+    console.log(e.target.trim == "http://localhost:5111/");
+    if (e.target =="http://localhost:5111/" ) {
 
-// Write your JavaScript code.
+    } else {
+
+    }
+});
