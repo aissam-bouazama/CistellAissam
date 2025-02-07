c5e2dd4c2a7d739767658584676752139a934515
Aissam <abouzama@almata.cat>
2025-02-07 17:29:45 +0100

    Reestructuración de controladores y vistas
    Se eliminaron todas las referencias y el contenido del archivo `HomeController.cs`, incluyendo los métodos `Index`, `Privacy` y `Error`.
    
    Se reemplazó el contenido del archivo `LayoutCistel.cshtml` con una nueva estructura HTML básica, estilos CSS para la barra lateral y enlaces a hojas de estilo y scripts externos.
    
    Se eliminaron los contenidos de los archivos `Index.cshtml` y `Privacy.cshtml`.
    
    Se añadió un nuevo controlador `ProducteController.cs` con un método `Index` que devuelve una vista llamada `AfegirProducte`.
    
    Se añadió un nuevo archivo `AfegirProducte.cshtml` que contiene un formulario para agregar un producto con campos para el código, nombre y precio del producto, así como un campo oculto para la imagen del producto.
    
    Se añadió una nueva entrada en la barra lateral del archivo `LayoutCistel.cshtml` para agregar un nuevo producto, que enlaza al método `Index` del `ProducteController`.

---
+++ CistellAissam/Controllers/ProducteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CistellAissam.Controllers
+{
+    public class ProducteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("AfegirProducte");
+        }
+    }
+}
