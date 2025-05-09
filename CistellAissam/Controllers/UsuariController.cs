62de72279b152ff4421f20f51db868de5c4f88ee
Aissam <bouazamaaissam@gmail.com>
2025-05-09 14:48:46 +0200

    Añadido controlador y vistas para gestión de usuarios
    Se ha implementado el controlador `UsuariController` con acciones
    para listar, ver detalles y editar usuarios. También se han creado
    las vistas `Index`, `Details` y `Edit` para soportar estas acciones.
    
    En el archivo `LayoutCistel.cshtml`, se añadió un enlace en el menú
    para gestionar usuarios y se modificó la visualización del usuario
    autenticado, mostrando su correo como enlace.
    
    Se actualizaron las dependencias en `UsuariController.cs` y la
    cadena de conexión en `appsettings.json` para incluir una contraseña.

--- CistellAissam/Controllers/UsuariController.cs
+++ CistellAissam/Controllers/UsuariController.cs
@@ -1,9 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CistellAissam.Data;
+using CistellAissam.Models;
+using CistellAissam.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 
 namespace CistellAissam.Controllers
 {
     public class UsuariController : Controller
     {
+        private readonly TiendaContext _DBContext;
+        public UsuariController(TiendaContext context)
+        {
+            this._DBContext = context;
+        }
+        public IActionResult Index()
+        {
+            ViewData["userauth"] = SessionUtils.ObtenerUsuariAuth(HttpContext);
+            var usuaris = _DBContext.usuaris.ToList();
+            return View("Index", usuaris);
+        }
+       public IActionResult Details(string id)
+        {
+            var usuari = _DBContext.usuaris.FirstOrDefault(x => x.Email == id);
+            return View("Details", usuari);
+        }
+        public IActionResult Edit(string id)
+        {
+            var usuari = _DBContext.usuaris.FirstOrDefault(x => x.Email == id);
+            return View("Edit", usuari);
+        }
+        [HttpPost]
+        public IActionResult Edit(Usuari usuari)
+        {
+            if (!ModelState.IsValid)
+            {
+                RedirectToAction("Edit", usuari);
+            }
+            var usuariBD = _DBContext.usuaris.FirstOrDefault(x => x.Email == usuari.Email);
+            if (usuariBD != null)
+            {
+                usuariBD.Nom = usuari.Nom;
+                usuariBD.Cognom = usuari.Cognom;
+                usuariBD.Telefon = usuari.Telefon;
+                usuariBD.DataNaixement = usuari.DataNaixement;
+                _DBContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
